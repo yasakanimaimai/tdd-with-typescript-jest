6f13687fd115067eded70b3eb22d51ba48c85ee5
fujitaiori <ioriori0413@gmail.com>
2022-09-28 00:09:01 +0900

    キャメルケース修正漏れ

--- chapter3/MoneyTest.test.ts
+++ chapter3/MoneyTest.test.ts
@@ -6,8 +6,8 @@ describe('', () => {
     const fiveDollar = new Dollar(5);
     const tenDollar = fiveDollar.times(2);
     expect(tenDollar.amount).toBe(10);
-    const fifteen_dolar = fiveDollar.times(3);
-    expect(fifteen_dolar.amount).toBe(15);
+    const fifteenDollar = fiveDollar.times(3);
+    expect(fifteenDollar.amount).toBe(15);
   });
 
   it('Equality', () => {
